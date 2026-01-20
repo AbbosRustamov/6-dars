901be8d745c401e1e4e9b2e8447b02775463ba10
AbbosRustamov <abbosrustamov0207@gmail.com>
2026-01-20 15:43:23 +0500

    done tasks

---
+++ Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+
+namespace dars
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+
+
+
+            /*int count = 1;
+
+
+            while(count <= 10)
+            {
+                Console.WriteLine(count);
+                count++;
+            }*/
+
+           /* string password = "";
+
+            while(password != "1234")
+            {
+                Console.Write("Parolni kiriting: ");
+                password = Console.ReadLine();
+            }
+            Console.WriteLine("Kirish muvaffaqiyatli!");
+            */
+            /*
+            int i = 1;
+
+            do
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+            while(i <= 5);
+            */
+
+            /*
+
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.WriteLine(i);
+            }
+            */
+            /*
+
+            for(int i = 1; i <= 10; i++)
+            {
+                if(i ==8)
+                break;
+
+                Console.WriteLine(i);
+            }
+            */
+            /*
+
+            for(int i = 1; i <= 5; i++)
+            {
+                if(i == 4)
+                continue;
+
+                Console.WriteLine(i);
+            }
+            */
+            /*
+
+            for(int i = 1; i <= 100; i += 2)
+            {
+                Console.WriteLine(i);
+            }
+            */
+
+            // 1- masala PDP online
+            /*
+            int K = 2;
+            int N = 3;
+
+            for(int i = 1; i <= N; i++)
+            {
+                Console.WriteLine(K);
+            }
+            */
+
+            // 2-masala 
+            /*
+
+            int num1 = 2;
+            int num2 = 4;
+            int sum = 0;
+
+            for(int i = num1 + 1; i < num2; i++)
+            {
+               sum += i;
+            }
+            
+            Console.WriteLine(sum);
+            */
+            // 3 - masala
+            /*
+
+            int n = 10;
+
+            for (int i = 1; i < n; i += 2)
+            {
+                Console.WriteLine(i);
+            }
+            */
+
+            // 4 - masala 
+
+
+
+        
+
+        }
+
+    }
+}
